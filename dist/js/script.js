2b6a5e5c360a034aef7f9aa54090915fcceded84
Bagja <bagja.basith0612@students.amikom.ac.id>
2022-10-08 13:13:26 +0700

    first add project sekolah pijar 2022

---
+++ dist/js/script.js
@@ -0,0 +1,65 @@
+// hamburger
+const hamburger = document.querySelector("#hamburger");
+const kotak = document.querySelector("#kotak");
+const kotak2 = document.querySelector("#kotak2");
+const x = document.querySelector("#x");
+//navMenu
+const navMenu = document.querySelector("#nav-menu");
+
+//navbarfixed
+window.onscroll = function () {
+  const header = document.querySelector("nav");
+
+  const fixednav = header.offsetTop;
+
+  if (window.pageYOffset > fixednav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+hamburger.addEventListener("click", function () {
+  kotak.classList.toggle("hidden");
+  x.classList.toggle("hidden");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger && e.target != kotak && e.target != kotak2) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+    x.classList.add("hidden");
+    kotak.classList.remove("hidden");
+  }
+});
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: "4",
+
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        biruMuda: "#67c7e0",
+        biruTua: "#16719F",
+        orangeMuda: "#F59024",
+        orangeTua: "#e58e42",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
